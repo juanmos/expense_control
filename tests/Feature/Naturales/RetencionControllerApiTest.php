27130197568d5b67a2ccd0c31e53097b816a25e9
JuanMoscoso <juan.moscoso@primme.tech>
2019-12-16 14:51:42 -0500

    refactor de test

---
+++ tests/Feature/Naturales/RetencionControllerApiTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature\Naturales;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Retencion;
+use App\Models\Institucion;
+use App\Models\User;
+use Crypt;
+
+class RetencionControllerApiTest extends TestCase
+{
+    use RefreshDatabase;
+    protected $headers;
+
+    public function setUp():void
+    {
+        parent::setUp();
+        factory(User::class)->create([
+            'email'    => 'test@email.com',
+            'password' => bcrypt('123456')
+        ]);
+        factory(Institucion::class)->create();
+        $token = auth()->guard('api')
+            ->login(User::first());
+        $this->headers['Authorization'] = 'Bearer ' . $token;
+    }
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_api_only_auth_users()
+    {
+        $response = $this->post('api/naturales/retenciones',[
+
+        ]);
+        $response->assertUnauthorized();
+    }
+
+    /** @test */
+    public function test_obtener_retenciones()
+    {
+        $response = $this->post('api/naturales/retenciones',[],$this->headers);;
+        $response->assertOk();
+        $json = (array)json_decode(Crypt::decryptString($response->getContent()));
+        
+        $this->assertArrayHasKey('retenciones',$json);
+        
+    }
+    
+}
